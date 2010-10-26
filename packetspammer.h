8061dc3e643e9d61c1cb24c7f60c15622dca8f61
Bob Copeland <me@bobcopeland.com>
2010-10-25 21:42:38 -0400

    Add missing typedef for ulong

--- packetspammer.h
+++ packetspammer.h
@@ -12,7 +12,7 @@
 typedef unsigned int u32;
 typedef unsigned short u16;
 typedef unsigned char u8;
-typedef u32 __le32;
+typedef unsigned long ulong;
 
 #if __BYTE_ORDER == __LITTLE_ENDIAN
 #define	le16_to_cpu(x) (x)
